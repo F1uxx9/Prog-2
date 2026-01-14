3dc3551923eac28c159ed7bfe9a76e4869e5af4b
F1uxx9 <eliasengstrom1234@gmail.com>
2026-01-14 10:33:34 +0000

    Tjena

---
+++ kalkylaten/src/App.java
@@ -0,0 +1,84 @@
+void main(){
+    while(true){
+        IO.println("\n--- Kalkylator ---");
+        IO.println("1. Addition");
+        IO.println("2. Subtraktion");
+        IO.println("3. Multiplikation");
+        IO.println("4. Division");
+        IO.println("5. Avsluta");
+        IO.println("Väljräknesätt");
+        
+
+        try {
+            String val = IO.readln();
+
+            if(val == "5"){
+                IO.println("Programmet har avslutats!");
+                continue;
+            }
+
+            IO.println("Ange det första talet.");
+            String tal1String = IO.readln();
+            int tal1 = 0;
+            try {
+            tal1 = Integer.parseInt(tal1String);
+            } catch (Exception e) {
+                IO.println("Du har inmatat ett fel värde.");
+            }
+
+            IO.println("Ange det andra talet.");
+            String tal2String = IO.readln();
+            int tal2 = 0;
+            try {
+            tal2 = Integer.parseInt(tal2String);
+            } catch (Exception e) {
+                IO.println("Du har inmatat ett fel värde.");
+            }
+
+            double resultat;
+
+            switch(val) {
+                case "1":
+                    resultat = addition(tal1, tal2);
+                    break;
+                case "2":
+                    resultat = subtraktion(tal1, tal2);
+                    break;
+                case "3":
+                    resultat = multiplikation(tal1, tal2);
+                    break;
+                case "4":
+                    resultat = division(tal1, tal2);
+                    break;
+                default:
+                    IO.println("Ogiltigt val.");
+                    continue;
+            }
+
+            IO.println("Resultat " + resultat);
+
+        } catch (Exception e) {
+            IO.println("Fel! Du måste skriva in ett tal. ");
+        }
+    }
+}
+
+double addition(double a, double b) {
+    return a + b;
+}
+
+double subtraktion(double a, double b) {
+    return a - b;
+}
+
+double multiplikation(double a, double b) {
+    return a * b;
+}
+
+double division(double a, double b){
+    if(b==0){
+        IO.println("Division med 0 fungerar inte!");
+        return 0;
+    }
+    return a / b;
+}
